1c2c1a3799209ca78c297d7b9c71c79d66069769
Christopher Conlon <cwconlon@gmail.com>
2024-05-05 20:16:56 -0700

    Added EPA and Pandas code

--- Module5/test.py
+++ Module5/test.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
 
+epa_data = pd.read_csv("/Users/cwconlon/@dev/stat_class/Notebooks/Module4/data/c4_epa_air_quality.csv", index_col = 0)
+
+print(epa_data.head(10))
 
 
 
